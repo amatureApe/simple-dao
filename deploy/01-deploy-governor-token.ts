5f3dfdd4a14cc9ec42be9ca2f96cb65fe7364af8
amatureApe <amatureApe@gmail.com>
2022-07-23 11:07:28 -0500

    add typescript and deployment setup

---
+++ deploy/01-deploy-governor-token.ts
@@ -0,0 +1,8 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const deployGovernanceToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  console.log("hello")
+}
+
+export default deployGovernanceToken;
