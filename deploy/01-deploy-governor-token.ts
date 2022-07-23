2c70aba59ef2f73b272a5787f3d385263aef4ddd
amatureApe <amatureApe@gmail.com>
2022-07-23 12:03:31 -0500

    add deploy-governor-token functionality

--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -1,8 +1,28 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
 
 const deployGovernanceToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  console.log("hello")
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  log("Deploying Governance Token...");
+  const governanceToken = await deploy("GovernanceToken", {
+    from: deployer,
+    args: [],
+    log: true,
+  });
+  log(`Deployed governance token to address ${governanceToken.address}`);
+
+  await delegate(governanceToken.address, deployer);
+  log("Delegated!")
+}
+
+const delegate = async (governanceTokenAddress: string, delegatedAccount: string) => {
+  const governanceToken = await ethers.getContractAt("GovernanceToken", governanceTokenAddress);
+  const tx = await governanceToken.delegate(delegatedAccount);
+  await tx.wait(1);
+  console.log(`Checkpoints ${await governanceToken.numCheckpoints(delegatedAccount)}`);
 }
 
 export default deployGovernanceToken;
