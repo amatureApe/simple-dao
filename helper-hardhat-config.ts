7c7140bd15701e98edeecd77effa9e7ccfbaa23a
amatureApe <amatureApe@gmail.com>
2022-07-24 13:00:45 -0500

    add propose.ts and related proposal functionality

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,10 +1,11 @@
-const { ethers } = require("hardhat");
+export const developmentChains = ["hardhat", "localhost"];
+export const proposalsFile = "proposals.json"
 
-const developmentChains = ["hardhat", "localhost"];
-const MIN_DELAY = 3600;
-const VOTING_PERIOD = 5;
-const VOTING_DELAY = 1;
-const QUORUM_PERCENTAGE = 4;
-const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
-
-module.exports = { developmentChains, MIN_DELAY, VOTING_PERIOD, VOTING_DELAY, QUORUM_PERCENTAGE, ADDRESS_ZERO }
+export const MIN_DELAY = 3600;
+export const VOTING_PERIOD = 5;
+export const VOTING_DELAY = 1;
+export const QUORUM_PERCENTAGE = 4;
+export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+export const FUNC = "store";
+export const NEW_STORE_VALUE = 69;
+export const PROPOSAL_DESCRIPTION = "Proposal #1: Store 69 in the Box";
